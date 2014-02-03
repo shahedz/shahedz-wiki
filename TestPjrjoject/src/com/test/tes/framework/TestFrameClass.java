88a62e493cbb63764029323f74a0665fbcf7084c
shahedz <z_yman@yahoo.com>
2014-02-03 12:47:16 -0500

    testRubel

--- TestPjrjoject/src/com/test/tes/framework/TestFrameClass.java
+++ TestPjrjoject/src/com/test/tes/framework/TestFrameClass.java
@@ -5,6 +5,12 @@ public class TestFrameClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		testTwo();
 	}
 
+	
+	public static void testTwo(){
+		System.out.println("hello world two");
+		
+	}
 }
