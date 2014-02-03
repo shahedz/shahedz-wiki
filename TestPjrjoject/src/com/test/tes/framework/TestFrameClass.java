19583f3c4e598113df680bfacb6bfc81c3abe334
shahedz <z_yman@yahoo.com>
2014-02-03 09:19:40 -0500

    why is there all the metadata?

---
+++ TestPjrjoject/src/com/test/tes/framework/TestFrameClass.java
@@ -0,0 +1,10 @@
+package com.test.tes.framework;
+
+public class TestFrameClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
